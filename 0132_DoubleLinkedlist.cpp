498a2e9b82eb969222a8314e71fdbf5cd915fdfd
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-05-15 15:21:05 +0700

    step 3

--- 0132_DoubleLinkedlist.cpp
+++ 0132_DoubleLinkedlist.cpp
@@ -32,5 +32,15 @@ public:
 
         //step 2: Assign value to the data fields
         newNode->noMhs = nim;
+
+        //step 3: Insert at beginning if list is empty or nimm is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+        }
     }
 }
