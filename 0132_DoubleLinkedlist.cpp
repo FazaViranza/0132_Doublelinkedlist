e8b222de04bb2aceef0d894025ea210a821ea8a1
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-05-15 15:16:53 +0700

    step 1 new node

--- 0132_DoubleLinkedlist.cpp
+++ 0132_DoubleLinkedlist.cpp
@@ -26,5 +26,8 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        //step 1: Allocate memory for new node
+        Node *newNode = new Node();
     }
 }
